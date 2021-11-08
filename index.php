65da38bd282d21fd808ebeaad960095bc87d14be
Hernando17 <ginkotsu17@gmail.com>
2021-11-08 13:07:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <?php
+    if (isset($_GET['pesan'])) {
+        if ($_GET['pesan'] == "gagal") {
+            echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+        }
+    }
+    ?>
+    <form action="cek_login.php" method="post">
+        <div class="mb-3">
+            <label for="exampleInputusername1" class="form-label">username</label>
+            <input type="username" class="form-control" id="exampleInputusername1" aria-describedby="usernameHelp" name="username">
+            <div id="usernameHelp" class="form-text">We'll never share your username with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+<footer>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</footer>
+
+</html>
